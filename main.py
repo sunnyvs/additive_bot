e0ff122085b97fca3387549fa611672dabf170ba
Ekaterina Zavaleeva <zavaka@bk.ru>
2025-04-09 17:14:59 +1000

    first try

---
+++ main.py
@@ -0,0 +1,40 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+import asyncio
+
+FILE_PATH = 'langeta.rar'  # замени на путь к твоему файлу
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("Получить файл", callback_data='send_file')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Нажмите кнопку, чтобы получить файл:", reply_markup=reply_markup)
+
+async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == 'send_file':
+
+        wait_msg = await query.message.reply_text("🔍 Подготовка файла...")
+
+        dots = ["⏳", "⌛", "⏳", "⌛"",⏳", "⌛"]
+        for dot in dots:
+            await asyncio.sleep(1)  # Задержка 1 секунда
+            await wait_msg.edit_text(f"Файл загружается {dot}")
+
+        with open(FILE_PATH, 'rb') as f:
+            await query.message.reply_document(document=f, filename=FILE_PATH.split("/")[-1])
+
+        await wait_msg.delete()
+
+
+if __name__ == '__main__':
+    TOKEN = "7709229308:AAEMYz2B2VYPlQ6yCFV-AkaB5aQ1QU2oosI"  # можно заменить на строку токена прямо
+
+    app = ApplicationBuilder().token(TOKEN).read_timeout(7).write_timeout(7).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(button_callback))
+    app.run_polling()
